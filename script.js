5e73181c1b5d60daa9da61b6b657a74e141b83c5
Gleibson Pereira <gleibson.santos05@gmail.com>
2021-08-10 16:57:59 -0300

    proximo comit

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ document.getElementById("titulo").innerHTML = "Opa, tudo bem"
 
 //escreve algo na tela
 document.write("algo na tela")
+
+//seleciona todos os elementos pelo nome da classe
+document.getElementsByClassName("")
+
+//seleciona todos os elementos pelo nome da tag
+document.getElementsByTagName("")
+
+//seleiona todos os elementos pelo atributo name, mais ultilizado em campos de texto
+document.getElementsByName("")
