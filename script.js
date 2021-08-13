22a68077076b9e6f895859d82107c668b0d03292
Gleibson Pereira <gleibson.santos05@gmail.com>
2021-08-13 17:38:05 -0300

    proximo comit

--- script.js
+++ script.js
@@ -75,8 +75,31 @@ function abrir(){
     }
 }
 
-// usando switche
+// usando switch
 
 let dia = 3
+let diaNome = ''
 
-
+switch (dia) {
+    case 1:
+        diaNome = 'Segunda-feira'
+        break
+    case 2:
+        diaNome = 'Terça-feira'
+        break
+    case 3:
+        diaNome = 'Quarta-feira'
+        break
+    case 4:
+        diaNome = 'Qinta-feira'
+        break
+    case 5:
+        diaNome = 'Sexta-feira'
+        break
+    case 6:
+        diaNome = 'Sábado'
+        break
+    case 7:
+        diaNome = 'Domingo'
+        break
+}
