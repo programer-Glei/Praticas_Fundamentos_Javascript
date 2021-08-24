1bb1995c2e810e97f34a4eee687fa6dc46c0f81b
Gleibson Pereira <gleibson.santos05@gmail.com>
2021-08-24 17:49:46 -0300

    proximo comit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //seleciona o elemento pelo id
-document.getElementById("titulo").innerHTML = "Opa, tudo bem"
+//document.getElementById("titulo").innerHTML = "Opa, tudo bem"
 
 //escreve algo na tela
 document.write("algo na tela")
